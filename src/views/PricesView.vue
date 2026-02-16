3351fe6ba22f41118512c5b0ebdb658368dc52ae
Prout <fausse.aseptik@gmail.com>
2026-02-16 17:26:17 +0100

    Bandeau info pricing personnel, pricing personnel > pricing collectif

--- src/views/PricesView.vue
+++ src/views/PricesView.vue
@@ -188,6 +188,18 @@
         </div>
       </div>
       
+      <!-- Info banner for personal prices -->
+      <div class="bg-blue-900/30 border-2 border-blue-700/50 rounded-lg p-4 mb-4">
+        <div class="flex items-start gap-3">
+          <svg class="w-6 h-6 text-blue-400 shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+          </svg>
+          <div class="flex-1">
+            <p class="text-sm text-blue-200">{{ $t('divers.prices_personal_info') }}</p>
+          </div>
+        </div>
+      </div>
+      
       <!-- Personal Prices Table -->
       <PricesTable
         tab-type="personal"
